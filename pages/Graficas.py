2fd5e7a7573da05dfaceddaad7335d941918bf3b
XimenaAmay123 <105090210+XimenaAmay123@users.noreply.github.com>
2023-05-03 22:20:08 -0600

    Update Graficas.py

--- pages/Graficas.py
+++ pages/Graficas.py
@@ -1,6 +1,6 @@
 import pandas as pd
 import streamlit as st
-import plotly as px
+import plotly.expres as px
 import numpy as np
 import plotly.graph_objects as go
 import streamlit.components.v1 as components
